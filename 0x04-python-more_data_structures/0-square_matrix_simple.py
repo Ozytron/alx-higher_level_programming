6da4a27ec78dc5c0c2475e2064145288f3ba2fc7
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-01-29 09:19:09 +0000

    add correction to code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
     for row in range(len(matrix)):
-        new_matrix = list(map(lambda x: x**2, matrix[row]))
+        new_matrix[row] = list(map(lambda x: x**2, matrix[row]))
 
     return (new_matrix)
