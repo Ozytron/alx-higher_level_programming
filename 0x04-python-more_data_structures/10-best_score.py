7a5fe99fe8d0c1533fbd788bb576e2798712ddc2
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-01-29 22:32:14 +0000

    add more correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     score = 0
-    if a_dictionary is None:
+    if not a_dictionary:
         return (None)
     for key in a_dictionary:
         if a_dictionary[key] > score:
