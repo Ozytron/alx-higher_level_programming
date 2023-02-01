fb3da548dd85a041af11fed5f58c2c3cfcfab7f4
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-02-01 19:06:06 +0000

    add correction to code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
 
     for element in range(x):
         try:
-            print("{}".format(my_list=[element]), end="")
+            print("{}".format(my_list[element]), end="")
             total_elements += 1
         except IndexError:
             break
