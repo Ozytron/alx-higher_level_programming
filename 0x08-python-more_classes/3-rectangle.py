ceba871fdd903044a4edc230410833df40673fb0
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-02-15 12:56:03 +0000

    add correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,6 +55,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return (0)
         return ((self.width * 2) + (self.height * 2))
+
     def __str__(self):
         """rectangle as #"""
         if self.width == 0 or self.height == 0:
