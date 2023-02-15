ca3af54d96f9feae599fbdabec41bb2f6d6706ca
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-02-15 09:21:40 +0000

    add correction to the class instances

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,19 +3,26 @@
 
 
 class Rectangle:
-    """initialization"""
+    """
+    Defines a rectangle by:
+    private instance attribute: width (int)
+    private instance attribute: height (int)
+    initializes width and height
+    """
+
     def __init__(self, width=0, height=0):
+        """initialization"""
         self.width = width
         self.height = height
 
     @property
-    """ width getter"""
     def width(self):
+        """ width getter"""
         return (self.__width)
 
     @width.setter
-    """width setter"""
     def width(self, value):
+        """width setter"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,13 +30,13 @@ class Rectangle:
         self.__width = value
 
     @property
-    """ Height getter"""
     def height(self):
+        """ Height getter"""
         return (self.__height)
 
     @height.setter
-    """ Height setter"""
     def height(self, value):
+        """ Height setter"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
