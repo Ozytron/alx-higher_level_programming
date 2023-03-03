5650f0b5ac86d3cd478644f38baf1d3ea83e2afd
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-03-03 09:01:19 +0000

    add new files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Lookup module"""
+
+
+def lookup(obj):
+    """function Returns: a list of available attributes
+       and methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""is the same module"""
+
+
+def is_same_class(obj, a_class):
+    """function returns True if the object is exactly an instance
+       of the specified class. 
+       otherwise: return False."""
+    return type(obj) == a_class
