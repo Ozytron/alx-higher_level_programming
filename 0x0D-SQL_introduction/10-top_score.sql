27d66341d40c7e9aed247f46268a0e7172edf325
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-03-15 07:36:13 +0000

    add new fix

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
--- List by best
--- Show score ordered and name from second_table
-SELECT score, name FROM second_table ORDER BY score DESC, name;
+-- Script that lists all records in a table
+-- Query to lists all records in the table second_table of a database
+SELECT score, name FROM second_table ORDER BY score DESC;
