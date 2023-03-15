693ac31f089788fcb5bf9e29684fa26e05909ed6
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-03-15 15:40:09 -0700

    add more files to creat tables with different constraints

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Script that creates the table on your MySQL server.
+-- Query that creates the table force_name on your MySQL server.
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL);
