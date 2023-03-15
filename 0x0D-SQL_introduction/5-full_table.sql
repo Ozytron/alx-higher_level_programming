c83dd803160a2b3c3b9e49beca875e3b909ff258
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-03-15 06:52:01 +0000

    add correction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
---script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
+--prints the full description of the table first_table from the database.
+--Query to print full description of a table
 SHOW CREATE TABLE first_table;
