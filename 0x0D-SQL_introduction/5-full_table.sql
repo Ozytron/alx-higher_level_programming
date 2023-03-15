04f11f66a11a54a7b9e75e79dfc348b9fb491f9a
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-03-15 07:45:31 +0000

    add more fixes

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
--- Prints the full description of the table first_table from the database
--- Query to print full description of a table
+-- Script prints the full description of a table.
+-- Query to print full description of a table first_table from the database.
 SHOW CREATE TABLE first_table;
