364798d1e20bc9c6ed11ddca502f83e6b6af4589
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-03-15 17:48:56 -0700

    add more fixes

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,5 +2,4 @@
 -- Query that creates the database hbtn_0d_usa and the table states
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(256) NOT NULL)
-    type=InnoDB;
+    name VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,6 @@
-- Script that creates the database and the table
+-- Script that creates the database and the table
 -- Query that creates the database hbtn_0d_usa and the table cities
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
-    name VARCHAR(256) NOT NULL)
-    type=InnoDB;
+    name VARCHAR(256) NOT NULL);
