d9567e9df18269c657916edb273b77852d6a8c65
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-04-11 14:56:06 -0700

    add correction

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function add (num1, num2) {
     return num1 + num2;
-  }
+}
   
-  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
